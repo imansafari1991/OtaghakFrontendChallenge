1dd68a1413bb7993dc15e72ba5b7b626671ad220
Amirreza Noori <ar.noori@u01.ir>
2024-04-18 17:33:01 +0330

    iransans font added,

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import localFont from 'next/font/local';
+
+const font = localFont({
+	src: '../assets/fonts/IRANSansWeb.woff2',
+  variable: '--font-iran-sans'
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={font.className}>
+			<Component {...pageProps} />;
+		</main>
+	);
 }
