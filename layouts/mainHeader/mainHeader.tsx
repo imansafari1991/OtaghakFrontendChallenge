e40a19486a698e9a4a3f4dcd29517d363f0f310f
Amirreza Noori <ar.noori@u01.ir>
2024-04-20 07:05:39 +0330

    site logo responsive added.

--- layouts/mainHeader/mainHeader.tsx
+++ layouts/mainHeader/mainHeader.tsx
@@ -4,17 +4,39 @@ import { Container } from '../container';
 import {
 	DownloadReceiveIcon,
 	HouseAddSeatIcon,
+	PhoneIcon,
 	SupportIcon,
 	UserProfileIcon,
 } from '@/icons';
+import Link from 'next/link';
 
 export default function MainHeader() {
-	const logoUrl =
-		'https://cdn.otaghak.com/otg-images-new/Web/Icon/general/logotype-large.svg';
 	return (
 		<header className='bg-white border-b border-tertiary-outlined-border'>
-			<Container className='flex h-20 items-center'>
-				<Image src={logoUrl} width={112} height={44} alt='Otaghak' />
+			<Container className='flex h-16 sm:h-20 items-center'>
+				<Link href='#'>
+					<Image
+						className='hidden sm:block'
+						src={'/otaghak.svg'}
+						width={112}
+						height={44}
+						alt='Otaghak'
+					/>
+					<Image
+						className='sm:hidden'
+						src={'/otaghak-mobile.svg'}
+						width={80}
+						height={32}
+						alt='Otaghak'
+					/>
+				</Link>
+				<Button
+					className='flex sm:hidden ms-auto'
+					href='#'
+					icon={<PhoneIcon />}
+				>
+					تماس با پشتیبانی
+				</Button>
 				<Button
 					className='ms-auto hidden sm:flex'
 					href='#'
